9ddd64241fc74ea0eca603b2f85daea0b39be983
hunght <bachhunght@gmail.com>
2016-06-06 08:20:38 +0700

    update theme sitewide

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -1,6 +1,7 @@
 /*jslint browser: true*/
 /*global $, jQuery, Modernizr, enquire*/
 (function (window, document, $) {
+
   var $html = $('html'),
     mobileOnly = "screen and (max-width:47.9375em)", // 767px.
     mobileLandscape = "(min-width:30em)", // 480px.
@@ -18,4 +19,48 @@
 
   // Add  functionality here.
 
+
 }(this, this.document, this.jQuery));
+
+
+/* function custom equalheight */
+
+equalheight = function(container){
+
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(container).each(function() {
+
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+}
+
+$(window).load(function() {
+  equalheight('.footer-column');
+});
+
+
+$(window).resize(function(){
+  equalheight('.footer-column');
+});
